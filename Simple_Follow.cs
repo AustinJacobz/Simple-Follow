c303ae43b75a7b75e7cab364fca1f9911dfe5450
AustinJacobz <75528509+AustinJacobz@users.noreply.github.com>
2020-12-08 19:54:50 -1000

    Added files.

---
+++ Simple_Follow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+[Serializable]
+public enum UpdateMethod
+{
+    PhysicsUpdate,
+    FrameUpdate,
+    LateUpdate
+}
+
+public class Simple_Follow : MonoBehaviour
+{
+    public Transform target; //Target you want to track.
+
+    public Vector3 offset; //Offset between Object and Target.
+    
+    [SerializeField] float smoothAmount = 0.2f; //Smooth Speed.
+
+    public UpdateMethod smoothOnTick; //Which Update type to smooth objects on.
+
+    private Vector3 velocity; //Current velocity calculated on UpdateMethod.
+
+    private Vector3 lastPos;
+    private Vector3 currentPos;
+
+    private void SmoothPos()
+    {
+        currentPos = target.position;
+        velocity = lastPos - currentPos; //Gets Velocity
+        Vector3 localTarget = target.position + offset; //Position to smooth to.
+        transform.position = Vector3.SmoothDamp(transform.position, localTarget, ref velocity, smoothAmount); //Smoothing to position.
+        lastPos = currentPos; //Resets last position.
+    }
+
+    private void FixedUpdate() //Physics Update.
+    {
+        if (smoothOnTick == UpdateMethod.PhysicsUpdate)
+        {
+            SmoothPos();
+        }
+    }
+    private void Update() //Graphics Update.
+    {
+        if (smoothOnTick == UpdateMethod.FrameUpdate)
+        {
+            SmoothPos();
+        }
+    }
+    private void LateUpdate() //Late Update.
+    {
+        if (smoothOnTick == UpdateMethod.LateUpdate)
+        {
+            SmoothPos();
+        }
+    }
+}
---
+++ Simple_FollowEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(FollowTarget))]
+public class Simple_FollowEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        FollowTarget myTarget = (FollowTarget)target;
+        if(GUILayout.Button("Get Offset"))
+        {
+            Vector3 newOffset = myTarget.transform.position - myTarget.target.position;
+            myTarget.offset = newOffset;
+        }
+    }
+}
